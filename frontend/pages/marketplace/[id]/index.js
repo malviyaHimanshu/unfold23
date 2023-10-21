2a84a50f5ae5dfaf39f6debf26bf5c5c2d8f883b
rp-ram <ramachakradharrp@gmail.com>
2023-10-21 06:47:50 +0530

    Votes in the marketplace

--- frontend/pages/marketplace/[id]/index.js
+++ frontend/pages/marketplace/[id]/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState,useEffect}from "react";
 import styles from "./styles.module.scss";
 const imgSrc =
   "https://unsplash.com/photos/a-video-game-controller-flying-through-the-air-B6oBCNG7IK4";
@@ -6,6 +6,21 @@ const imgSrc =
 // import a from "../../../components/mCard/sheeryImage.png";
 
 const CardDetail = () => {
+  const [number, setNumber] = useState(5);
+  useEffect(() => {
+    // Generate a random number between 1 and 10 and set it as the initial state
+    const randomInitialNumber = Math.floor(Math.random() * 102) + 1;
+    setNumber(randomInitialNumber);
+  }, []);
+  const handleIncrement = () => {
+    // Increase the number by 1 when the up arrow is clicked
+    setNumber(number + 1);
+  };
+
+  const handleDecrement = () => {
+    // Decrease the number by 1 when the down arrow is clicked
+    setNumber(number - 1);
+  };
   return (
     <div className={styles.main}>
       <div className={styles.cardPreview}>
@@ -35,9 +50,31 @@ const CardDetail = () => {
           commodi quo, laboriosam doloremque rem!
         </p>
         <br />
-
         <div className="">
-          <h3>Open Edition</h3>
+        <h3>Open Edition</h3>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <button
+            style={{ background: "none", border: "none" }}
+            onClick={handleIncrement}
+          >
+            <img
+              className={styles.Image}
+              src="https://cdn.icon-icons.com/icons2/3391/PNG/512/small_up_arrow_icon_213613.png"
+              alt="uparrow"
+            />
+          </button>
+          <h2>{number}</h2>
+          <button
+            style={{ background: "none", border: "none" }}
+            onClick={handleDecrement}
+          >
+            <img
+              className={styles.Image}
+              src="https://cdn.icon-icons.com/icons2/3389/PNG/512/arrow_small_down_icon_213264.png"
+              alt="downarrow"
+            />
+          </button>
+        </div>
           <br />
           <div className={styles.box}>
             <h2 className="price">ETH 0.231</h2>
